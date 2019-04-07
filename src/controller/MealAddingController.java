c0f3dacaddae5334db4347c834bc7015eed0a73c
Moh. Al-Ahmed <mohammedfbi63@gmail.com>
2019-04-08 01:24:28 +0300

    all shit2

---
+++ src/controller/MealAddingController.java
@@ -0,0 +1,73 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
+
+import java.awt.*;
+
+public class MealAddingController {
+    @FXML
+    private MenuItem HighestProtienBoxObject;
+    @FXML
+    private MenuItem LowestProBoxObject;
+    @FXML
+    private MenuItem HighestCaloriesBoxObject;
+    @FXML
+    private MenuItem LowesttCaloriesBoxObject;
+    @FXML
+    private CheckBox theFoodBoxId;
+    @FXML
+    private CheckBox BevarageBoxID;
+    @FXML
+    private MenuItem ZtoABoxObject;
+    @FXML
+    private MenuItem AtoZBoxObject;
+    @FXML
+    private TextField searchingTextObject;
+
+
+    public void SearchFieldHandeling()
+    {
+
+    }
+
+    public void AtoZCheckBox()
+    {
+
+    }
+    public void ZtoACheckBox()
+    {
+
+    }
+    public void HighestProtienCheckBox()
+    {
+
+    }
+    public void LowestProtienCheckBox()
+    {
+
+    }
+    public void HighestCaloriesCheckBox()
+    {
+
+    }
+    public void LowestCaloriesCheckBox()
+    {
+
+    }
+    public void bevarageActionBox()
+    {
+
+    }
+    public void foodActionBox()
+    {
+
+    }
+
+    public void BevarageActionBox()
+    {
+
+    }
+}
